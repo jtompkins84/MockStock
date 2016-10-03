f4404056c6ea8fa2891c4c4adbf209f9f02d08ac
jtompkins84 <joseph.tompkins@mavs.uta.edu>
2016-10-02 23:40:24 -0500

    Removed Fab button from Java code

--- app/src/main/java/com/cse4322/mockstock/MainActivity.java
+++ app/src/main/java/com/cse4322/mockstock/MainActivity.java
@@ -20,13 +20,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Refreshing Stocks", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-            }
-        });
 
         YahooFinanceService.updateStocks();
     }
