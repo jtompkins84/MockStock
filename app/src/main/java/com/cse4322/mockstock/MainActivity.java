27814d826afe11985e3695e0a900e835f4135a3b
jtompkins84 <joseph.tompkins@mavs.uta.edu>
2016-10-05 09:27:34 -0500

    Fixed an issue with YahooFinanceService causing a crash

--- app/src/main/java/com/cse4322/mockstock/MainActivity.java
+++ app/src/main/java/com/cse4322/mockstock/MainActivity.java
@@ -1,15 +1,10 @@
 package com.cse4322.mockstock;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import yahoofinance.YahooFinance;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-        YahooFinanceService.updateStocks();
+        YahooFinanceService.getNASDAQList();
     }
 
     @Override
--- app/src/main/java/com/cse4322/mockstock/YahooFinanceService.java
+++ app/src/main/java/com/cse4322/mockstock/YahooFinanceService.java
@@ -19,6 +19,7 @@ import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
 
 import yahoofinance.*;
@@ -30,19 +31,27 @@ public class YahooFinanceService {
     public static final String nasdaqDir = "/SymbolDirectory";
     public static final String nasdaqFile = "nasdaqlisted.txt";
 
+    private static int maxSize = 4500;
     /**
      * symbols stores the ticker symbols & company name as Pair<String, String>
      * for the particular market of interest. Since the stocks are pulled in alphabetically, this could
      * be used to implement search by name.
      */
-    public static ArrayList<StockPair> stocks = new ArrayList<>(5000);
+    private static ArrayList<StockPair> stocks = new ArrayList<StockPair>(maxSize);
     private static Map<String, Stock> stocksByTicker; // TODO implement map for searching stocks.
     private static Map<String, Stock> stocksByName = new HashMap<>(); // TODO implement map for searching stocks.
     private static AsyncTask updateTask;
+    private static String fileString; // a temporary variable for storing a file as a string. Only way to fit the file in memory.
+
     /**
+     * Compiles a list of ticker symbols and company names of stocks on the NASDAQ exchange,
+     * and returns in the form of <code>ArrayList<>StockPair></code>
      *
+     * @return an <code>ArrayList</code> that contains <code>StockPair</code> elements. Each
+     * <code>StockPair</code> contains the ticker symbol and company name of a stock on the NASDAQ exchange.
      */
-    public static void updateStocks() {
+    public static ArrayList<StockPair> getNASDAQList() {
+        fileString = new String();
         /*
         TODO Citation
         Code retrieved from:
@@ -54,8 +63,6 @@ public class YahooFinanceService {
 
             @Override
             protected Void doInBackground(String... params) {
-                StringTokenizer strtok;
-                String str;
                 try {
                     // Make FTP connection. Using org.apache.commons.net imported library.
                     FTPClient ftpClient = new FTPClient();
@@ -70,29 +77,42 @@ public class YahooFinanceService {
                     BufferedReader in = new BufferedReader(reader);
 
                     in.readLine(); // skip first line. First line on NASDAQ txt file is not stock data.
+                    String temp = new String();
+                    // read entire file and concat to str
+                    while( (temp = in.readLine()) != null) {
+                        fileString += temp;
+                        fileString += "\n"; // put new line at end of each stock in file
+                    }
 
+                    StringTokenizer strtok;
+                    // initialize tokenizer for the file
+                    strtok = new StringTokenizer(fileString, "|");
                     // reads the entire file of stocks and stores the
                     int i = 0;
-                    while((str = in.readLine()) != null) {
-                        strtok = new StringTokenizer(str, "|");
+                    while(i < maxSize && strtok.hasMoreTokens()) {
                         // Gets the symbol and company name from the file.
                         // NOTE: this is only really working based on the file from the NASDAQ ftp.
                         //          May not work the same once other stock exchanges are implemented.
-                        stocks.add( new StockPair(strtok.nextToken(), strtok.nextToken()) );
-                        Log.d(this.getClass().getSimpleName(), stocks.get(i).getTickerSymbol() + " :: " + stocks.get(i).getCompanyName());
+                        stocks.add(new StockPair(strtok.nextToken("|"), strtok.nextToken("-|")) );
+                        if(strtok.hasMoreTokens()) strtok.nextToken("\n");
+                        else break;
+
+                        Log.d("YahooFinanceService", stocks.get(i).getTickerSymbol() + " :: " + stocks.get(i).getCompanyName());
                         i++;
                     }
 
-                    ftpClient.disconnect();
                     in.close();
+                    ftpClient.disconnect();
                 } catch (MalformedURLException e) {
                     Log.e("YahooFinanceService", e.getLocalizedMessage());
                 } catch (IOException e) {
                     Log.e("YahooFinanceService", e.getLocalizedMessage());
                 }
-
                 return null;
             }
         }.execute();
+
+        fileString = null; // insures fileString is trashed when getNASDAQList is complete
+        return stocks;
     }
 }
