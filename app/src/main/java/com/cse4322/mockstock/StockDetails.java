3cceeb11f13dc5b56148abddc5dea857bd5392ef
Winston <winston.suryana@gmail.com>
2016-10-08 22:03:52 -0500

    Create stock page view but detail text views need revision for appearance

---
+++ app/src/main/java/com/cse4322/mockstock/StockDetails.java
@@ -0,0 +1,13 @@
+package com.cse4322.mockstock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StockDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock_details);
+    }
+}
