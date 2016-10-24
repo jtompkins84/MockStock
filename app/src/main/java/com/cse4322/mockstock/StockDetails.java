89895fbde44ffe9e45fff4c48b673425cb883920
jtompkins84 <joseph.tompkins@mavs.uta.edu>
2016-10-24 14:43:38 -0500

    - Fixed the issue on the Stock Detail activity causing that weird gap between the tool bar and the status bar at the top

--- app/src/main/java/com/cse4322/mockstock/StockDetails.java
+++ app/src/main/java/com/cse4322/mockstock/StockDetails.java
@@ -1,8 +1,11 @@
 package com.cse4322.mockstock;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.WindowDecorActionBar;
 import android.support.v7.widget.Toolbar;
+import android.view.Window;
 import android.widget.TextView;
 
 
@@ -12,9 +15,18 @@ public class StockDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock_content);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbardetails);
-//        setSupportActionBar(toolbar);
 
+        WindowDecorActionBar winDecorActBar;
+
+        Toolbar toolbar = null;
+
+        try {
+            toolbar = (Toolbar) findViewById(R.id.mockstock_toolbar);
+
+            setSupportActionBar(toolbar);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         String companyname = getIntent().getExtras().getString("name");
         String ticker = getIntent().getExtras().getString("ticker");
